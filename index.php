33622e43333fcf3c3a7574a89066c0f06bd1c170
jay <scpl.jay@gmail.com>
2019-05-23 23:32:13 +0530

    html test code for download image or file

---
+++ index.php
@@ -0,0 +1,89 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Jekyll v3.8.5">
+    <title>Download File Code</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.3/examples/starter-template/">
+
+    <!-- Bootstrap core CSS -->
+	<link href="https://getbootstrap.com/docs/4.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+    <!-- Custom styles for this template -->
+    <link href="starter-template.css" rel="stylesheet">
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+	  <a class="navbar-brand" href="#">Download File</a>
+	</nav>
+
+<main role="main" class="container">
+	<br>
+	<br>
+	<br>
+	<div class="row">
+		<div class="col">
+			<div class="card" style="width: 18rem;">
+			  <img src="nature.jpg" class="card-img-top" alt="...">
+			  <div class="card-body">
+			    <h5 class="card-title">Nature 1</h5>
+			   
+			    <a href="nature.jpg" class="btn btn-primary" download="nature11">Download</a>
+			  </div>
+			</div>
+
+		</div>
+		<div class="col">
+			<div class="card" style="width: 18rem;">
+			  <img src="nature2.jpg" class="card-img-top" alt="...">
+			  <div class="card-body">
+			    <h5 class="card-title">Nature 2</h5>
+			    
+			    <a href="nature.jpg" class="btn btn-primary" download="nature22">Download</a>
+			  </div>
+			</div>
+		</div>
+		<div class="col">
+			<div class="card" style="width: 18rem;">
+			  <img src="nature3.jpg" class="card-img-top" alt="...">
+			  <div class="card-body">
+			    <h5 class="card-title">Nature 3</h5>
+			   
+			    <a href="nature3.jpg" class="btn btn-primary" download="nature33">Download</a>
+			  </div>
+			</div>
+		</div>
+	</div>
+  	
+
+	
+
+</main><!-- /.container -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script>window.jQuery || document.write('<script src="https://getbootstrap.com/docs/4.3/assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+<script src="https://getbootstrap.com/docs/4.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-xrRywqdh3PHs8keKZN+8zzc5TX0GRTLCcmivcbNJWm2rs5C8PRhcEn3czEjhAO9o" crossorigin="anonymous"></script>
+  </body>
+</html>
+
